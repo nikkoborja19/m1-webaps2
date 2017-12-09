3ada63b2d1e05050e4f0182e46b34a598ba47988
nikkoborja19 <nikkoborja19@yahoo.com>
2017-12-09 17:15:13 +0800

    new

---
+++ WEBAPS2/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('client'));
+
+
+app.listen(3000, function(){
+	console.log("server is running");
+});
+
+var express = require("express");
+var router = require("./router.js");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('client'));
+app.use("/api", router);
+app.use("*", function(req, res) {
+    res.send("Resource not found (404).");
+});
+
+app.listen(3000, function() {
+    console.log("Server started at PORT 3000");
+});
